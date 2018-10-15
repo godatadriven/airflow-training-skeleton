8a0bbf47d2f578c77c491adddf2b631534fc4548
Fokko Driesprong <fokkodriesprong@godatadriven.com>
2018-10-15 20:52:27 +0200

    Make Flake8 compliant

--- other/dataflow_job.py
+++ other/dataflow_job.py
@@ -40,7 +40,22 @@ def run(argv=None):
                 "result_table",
                 dataset="result_dataset",
                 project="gdd-airflow-training",
-                schema="city:string, county:string, district:string, duration:string, locality:string, newly_built:boolean, paon:string, postcode:string, ppd_category_type:string, price:numeric, property_type:string, record_status:string, saon:string, street:string, transaction:string, transfer_date:numeric",
+                schema="city:string, "
+                       "county:string, "
+                       "district:string, "
+                       "duration:string, "
+                       "locality:string, "
+                       "newly_built:boolean, "
+                       "paon:string, "
+                       "postcode:string, "
+                       "ppd_category_type:string, "
+                       "price:numeric, "
+                       "property_type:string, "
+                       "record_status:string, "
+                       "saon:string, "
+                       "street:string, "
+                       "transaction:string, "
+                       "transfer_date:numeric",
                 create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
             )
         )
