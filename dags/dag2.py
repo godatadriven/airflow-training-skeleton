f7c5012766983edcdd50c623a9a12b50c71740ed
Fokko Driesprong <fokko@driesprong.frl>
2019-02-24 15:37:28 +0100

    Fix line endings

--- dags/dag2.py
+++ dags/dag2.py
@@ -1,34 +1,32 @@
-import airflow​
-from airflow import DAG​
-from airflow.operators.bash_operator import BashOperator​
-from airflow.operators.dummy_operator import DummyOperator​
-from airflow.operators.python_operator import PythonOperator​
-​
-args = {"owner": "godatadriven", "start_date": airflow.utils.dates.days_ago(14)}​
-​
-dag = DAG(​
-    dag_id=”exercise2",​
-    default_args=args,​
-    description="Sample DAG showing some Airflow Operators.",​
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+args = {"owner": "godatadriven", "start_date": airflow.utils.dates.days_ago(14)}
+
+dag = DAG(
+    dag_id=”exercise2",
+    default_args=args,
+    description="Sample DAG showing some Airflow Operators."
 )​
-​
-​
-def _print_exec_date(execution_date, **context):​
+
+def _print_exec_date(execution_date, **context):
     print(execution_date)​
-​
-​
-print_date = PythonOperator(​
-    task_id="print_execution_date",​
-    python_callable=_print_exec_date,​
-    provide_context=True,​
-    dag=dag,​
-)​
-​
-end = DummyOperator(task_id="the_end", dag=dag)​
-​
-wait_tasks = [​
-    BashOperator(task_id=f"wait_{w}", bash_command=f"sleep {w}", dag=dag)​
-    for w in [1, 5, 10]​
-]​
-​
-print_date >> wait_tasks >> end​
+
+print_date = PythonOperator(
+    task_id="print_execution_date",
+    python_callable=_print_exec_date,
+    provide_context=True,
+    dag=dag,
+)
+
+end = DummyOperator(task_id="the_end", dag=dag)
+
+wait_tasks = [
+    BashOperator(task_id=f"wait_{w}", bash_command=f"sleep {w}", dag=dag)
+    for w in [1, 5, 10]
+]
+
+print_date >> wait_tasks >> end
