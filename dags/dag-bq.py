30069fde2683f7669498ea5db195786c8748b2e3
Fokko Driesprong <fokko@driesprong.frl>
2019-02-25 15:18:52 +0100

    Cleanup

---
+++ dags/dag-bq.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from datetime import datetime
+
+from bigquery_get_data import BigQueryGetDataOperator
+
+dag = DAG(
+    dag_id='godatafest',
+    schedule_interval='@daily',
+    default_args={
+        'owner': 'GoDataDriven',
+        'start_date': airflow.utils.dates.days.ago(2)
+    }
+)
+
+bq_fetch_data = BigQueryGetDataOperator(
+    task_id='bq_fetch_data',
+    sql="""
+    SELECT committer.name, count(1) as cnt 
+    FROM [bigquery-public-data.github_repos.commits]
+    WHERE DATE(committer.date) = '{{ dt }}'
+    AND repo_name LIKE '%airflow%' 
+    GROUP BY committer.name
+    LIMIT 5;
+""",
+    dag=dag
+)
+
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+
+
+def send_to_slack_func(**context):
+    ...
+
+
+send_to_slack = PythonOperator(
+    task_id='send_to_slack',
+    python_callable=send_to_slack_func,
+    provide_context=True,
+    dag=dag,
+)
+
+bq_fetch_data >> send_to_slack
