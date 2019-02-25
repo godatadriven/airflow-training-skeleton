1f136fda76e84362267677c8995d0e5d6e2be414
Fokko Driesprong <fokko@driesprong.frl>
2019-02-25 15:29:29 +0100

    .

--- dags/dag-bq.py
+++ dags/dag-bq.py
@@ -2,18 +2,18 @@ import airflow
 
 from airflow import DAG
 from datetime import datetime
+from airflow.utils.dates import days_ago
 
 dag = DAG(
     dag_id='godatafest',
     schedule_interval='@daily',
     default_args={
         'owner': 'GoDataDriven',
-        'start_date': airflow.utils.dates.days.ago(2)
+        'start_date': airflow.utils.dates.days_ago(2)
     }
 )
 
 from bigquery_get_data import BigQueryGetDataOperator
-
 
 bq_fetch_data = BigQueryGetDataOperator(
     task_id='bq_fetch_data',
