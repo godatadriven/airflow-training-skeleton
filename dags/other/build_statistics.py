7dd3835b98cd8ed555e8cf809985f6a31926b153
Fokko Driesprong <fokko@driesprong.frl>
2019-02-25 23:50:01 +0100

    Add print

--- dags/other/build_statistics.py
+++ dags/other/build_statistics.py
@@ -3,6 +3,8 @@ import sys
 from pyspark.sql.functions import col
 from pyspark.sql import SparkSession
 
+print(sys.argv)
+
 input_properties = sys.argv[1]
 input_currencies = sys.argv[2]
 target_path = sys.argv[3]
