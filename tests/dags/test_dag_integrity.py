96de2d8618e4f358a40c8757a06f58e6a40b98a9
Bas Harenslak <basharenslak@gmail.com>
2020-01-30 22:18:02 +0100

    Check for cycles in the DAG integrity test

--- tests/dags/test_dag_integrity.py
+++ tests/dags/test_dag_integrity.py
@@ -13,14 +13,6 @@ from airflow.utils.dag_processing import list_py_file_paths
 DAG_BASE_DIR = path.join(path.dirname(__file__), "..", "..", "dags")
 DAG_PATHS = list_py_file_paths(DAG_BASE_DIR, safe_mode=True, include_examples=False)
 
-@pytest.mark.parametrize("dag_path", DAG_PATHS)
-def test_dag_integrity(dag_path):
-    """Import DAG file and check for a valid DAG instance."""
-
-    dag_name = path.basename(dag_path)
-    module = _import_file(dag_name, dag_path)
-    assert any(isinstance(var, airflow_models.DAG) for var in vars(module).values())
-
 
 def _import_file(module_name, module_path):
     import importlib.util
@@ -30,3 +22,18 @@ def _import_file(module_name, module_path):
     spec.loader.exec_module(module)
 
     return module
+
+
+@pytest.mark.parametrize("dag_path", DAG_PATHS)
+def test_dag_integrity(dag_path):
+    """Import DAG file and check for a valid DAG instance."""
+
+    dag_name = path.basename(dag_path)
+    module = _import_file(dag_name, dag_path)
+
+    dag_objects = [var for var in vars(module).values() if isinstance(var, airflow_models.DAG)]
+    assert dag_objects
+
+    for dag in dag_objects:
+        dag.test_cycle()
+
